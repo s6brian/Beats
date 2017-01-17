cb7b2500566f70875f38bc2a3b4bdd974995e50d
Brian Tria <s6.brian.tria@gmail.com>
2017-01-17 14:58:18 +0800

    project setup

---
+++ Assets/Scripts/GenericClasses/SingletonBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonBehaviour<T> : MonoBehaviour
+{
+    private static SingletonBehaviour<T> m_instance = null;
+    public static SingletonBehaviour<T> Instance{ get{ return m_instance; }}
+
+    protected void Awake()
+    {
+        if( m_instance != null )
+        {
+            // ensure that there's only one instance of class T per scene
+            Destroy( this.gameObject );
+            return;
+        }
+
+        m_instance = this;
+    }
+}
